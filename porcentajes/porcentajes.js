38a1ad4166e5e817ce4e0f047f05123547763a66
Azul Petrocelli <azulpetrocelli81@gmail.com>
2022-09-01 13:35:55 -0300

    Cambiando los nombres a las variables y agregando mas funciones al boton

--- porcentajes/porcentajes.js
+++ porcentajes/porcentajes.js
@@ -1,11 +1,22 @@
-const precio = document.querySelector('#precio');
-const descuento = document.querySelector('#descuento');
-const botonDescuento = document.querySelector('.boton-descuento');
-const monto =  document.querySelector('.monto');
+const inputPrice = document.querySelector('#price');
+const inputDiscount = document.querySelector('#discount');
+const discountButton = document.querySelector('.discount-button');
+const pResult =  document.querySelector('.result');
 
-botonDescuento.addEventListener('click', calcularMonto);
+discountButton.addEventListener('click', calcularMonto);
 
 function calcularMonto() {
-  resultado = ( precio.value * (100 - descuento.value)) / 100;
-  monto.innerText = '$' + resultado;
+  const price = inputPrice.value;
+  const discount = inputDiscount.value;
+
+  let result = ( price * (100 - discount)) / 100;
+
+  if (price == "" || discount == "") {
+    alert("You have to put both values")
+  } else if (discount >= 100 ){
+    alert("The discount can't be greater than 100")
+  } else {
+    pResult.innerText = '$' + result;
+  }
+  
 }
