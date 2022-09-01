5393ed803e48d0e76939b5c8e14fead517e4c4ed
Azul Petrocelli <azulpetrocelli81@gmail.com>
2022-09-01 10:30:34 -0300

    Circulo Hecho

--- math.js
+++ math.js
@@ -1,12 +1,14 @@
+// Cuadrado
 function calcularCuadrado(lado) {
   return {
     perimetro: lado * 4,
-    area: lado * lado,
+    area: Math.pow(lado, 2),
   }
 }
 
 console.log(calcularCuadrado(5));
 
+// Triangulo
 function calcularTriangulo(lado1, lado2, base, altura) {
   return {
     perimetro: base + lado1 + lado2,
@@ -15,3 +17,16 @@ function calcularTriangulo(lado1, lado2, base, altura) {
 }
 
 console.log(calcularTriangulo( 6, 6, 4, 5));
+
+// Circulo
+function calcularCirculo(radio) {
+  const diametro = radio * 2;
+  const PI = Math.PI;
+
+  return {
+    circunferencia: diametro * PI,
+    area: (radio ** 2) * PI,
+  }
+}
+
+calcularCirculo(3)
